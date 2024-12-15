4288921a35c40ad97ac234bca439b3125a1b76c2
Fidarit Mullayanov <fidarit.mullayanov@yandex.ru>
2024-12-15 21:02:18 +0500

    ref: Подготовка DI

---
+++ SegmentFilter/App.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SegmentFilter;
+
+
+public class App(MainWindow mainWindow) : Application
+{
+    private readonly MainWindow mainWindow = mainWindow;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+}
---
+++ SegmentFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SegmentFilter;
+
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<App>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+
+        var app = host.Services.GetService<App>();
+        app?.Run();
+    }
+}
